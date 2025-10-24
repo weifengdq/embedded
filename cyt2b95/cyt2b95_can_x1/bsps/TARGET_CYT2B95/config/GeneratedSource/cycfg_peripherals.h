35f70679a7701f8eff2e7a0d3713394c18cb9175
weifengdq <weifengdq@163.com>
2025-10-24 17:41:19 +0800

    cyt2b95 can x1 only uart print

---
+++ cyt2b95/cyt2b95_can_x1/bsps/TARGET_CYT2B95/config/GeneratedSource/cycfg_peripherals.h
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * File Name: cycfg_peripherals.h
+ *
+ * Description:
+ * Peripheral Hardware Block configuration
+ * This file was automatically generated and should not be modified.
+ * Configurator Backend 3.60.0
+ * device-db 4.31.0.9165
+ * mtb-pdl-cat1 3.18.0.43833
+ *
+ *******************************************************************************
+ * Copyright 2025 Cypress Semiconductor Corporation (an Infineon company) or
+ * an affiliate of Cypress Semiconductor Corporation.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#if !defined(CYCFG_PERIPHERALS_H)
+#define CYCFG_PERIPHERALS_H
+
+#include "cycfg_notices.h"
+#include "cy_scb_uart.h"
+#include "cy_sysclk.h"
+
+#if defined (CY_USING_HAL)
+#include "cyhal_hwmgr.h"
+#include "cyhal.h"
+#endif /* defined (CY_USING_HAL) */
+
+#if defined (COMPONENT_MTB_HAL)
+#include "mtb_hal.h"
+#include "cycfg_clocks.h"
+#include "mtb_hal_hw_types.h"
+#endif /* defined (COMPONENT_MTB_HAL) */
+
+#if defined (CY_USING_HAL_LITE)
+#include "cyhal_hw_types.h"
+#endif /* defined (CY_USING_HAL_LITE) */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+#define scb_0_ENABLED 1U
+#define scb_0_HW SCB0
+#define scb_0_IRQ scb_0_interrupt_IRQn
+
+extern const cy_stc_scb_uart_config_t scb_0_config;
+
+#if defined (CY_USING_HAL) || defined(CY_USING_HAL_LITE)
+extern const cyhal_resource_inst_t scb_0_obj;
+#endif /* defined (CY_USING_HAL) || defined(CY_USING_HAL_LITE) */
+
+#if defined(CY_USING_HAL_LITE) || defined (CY_USING_HAL)
+extern const cyhal_clock_t scb_0_clock;
+#endif /* defined(CY_USING_HAL_LITE) || defined (CY_USING_HAL) */
+
+#if defined (CY_USING_HAL) || defined(CY_USING_HAL_LITE)
+extern const cyhal_uart_configurator_t scb_0_hal_config;
+#endif /* defined (CY_USING_HAL) || defined(CY_USING_HAL_LITE) */
+
+#if defined (COMPONENT_MTB_HAL)
+extern const mtb_hal_peri_div_t scb_0_clock_ref;
+extern const mtb_hal_clock_t scb_0_hal_clock;
+#endif /* defined (COMPONENT_MTB_HAL) */
+
+#if defined (COMPONENT_MTB_HAL) && (MTB_HAL_DRIVER_AVAILABLE_UART)
+extern const mtb_hal_uart_configurator_t scb_0_hal_config;
+#endif /* defined (COMPONENT_MTB_HAL) && (MTB_HAL_DRIVER_AVAILABLE_UART) */
+
+void init_cycfg_peripherals(void);
+void reserve_cycfg_peripherals(void);
+
+#if defined(__cplusplus)
+}
+#endif /* defined(__cplusplus) */
+
+#endif /* CYCFG_PERIPHERALS_H */
--- cyt2b95/cyt2b95_can_x1/main.c
+++ cyt2b95/cyt2b95_can_x1/main.c
@@ -1,104 +1,39 @@
-/*******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the Empty Application Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*******************************************************************************
-* Copyright 2024-2025, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Header Files
-*******************************************************************************/
-#include "mtb_hal.h"
+#include "cy_pdl.h"
+#include "cy_scb_uart.h"
 #include "cybsp.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
-/*******************************************************************************
-* Macros
-*******************************************************************************/
+cy_stc_scb_uart_context_t uart0;
+uint8_t g_uart_out_data[256];
 
-
-/*******************************************************************************
-* Global Variables
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Definitions
-*******************************************************************************/
-
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
-int main(void)
-{
-    cy_rslt_t result;
-
-    /* Initialize the device and board peripherals */
-    result = cybsp_init();
-
-    /* Board init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
-
-    /* Enable global interrupts */
-    __enable_irq();
-
-    for (;;)
-    {
-    }
+void print(void *fmt, ...) {
+  va_list arg;
+  va_start(arg, fmt);
+  vsprintf((char *)&g_uart_out_data[0], (char *)fmt, arg);
+  while (Cy_SCB_UART_IsTxComplete(SCB0) != true) {
+  };
+  Cy_SCB_UART_PutArray(SCB0, g_uart_out_data, strlen((char *)g_uart_out_data));
+  va_end(arg);
 }
 
-/* [] END OF FILE */
+int main(void) {
+  cy_rslt_t result = cybsp_init();
+  if (result != CY_RSLT_SUCCESS) {
+    CY_ASSERT(0);
+  }
+  __enable_irq();
+
+  Cy_SCB_UART_DeInit(SCB0);
+  Cy_SCB_UART_Init(SCB0, &scb_0_config, &uart0);
+  Cy_SCB_UART_Enable(SCB0);
+
+  print("CYT2B95 CAN x1\r\n");
+
+  while (1) {
+    // Your application code here
+  }
+
+  return 0;
+}
