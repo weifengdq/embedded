16adefc31592c99295cff366f3241408af678cfb
weifengdq <weifengdq@163.com>
2025-11-19 13:17:35 +0800

    a503 blink ok

--- gd32a503/a503_blink/Source/main.c
+++ gd32a503/a503_blink/Source/main.c
@@ -6,6 +6,17 @@
 int main(void) {
   systick_config();
 
+  // PC0 LED
+  rcu_periph_clock_enable(RCU_GPIOC);
+  gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_0);
+  gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0);
+  // 低电平熄灭, 以下方法都可以
+  // GPIO_BCR(GPIOC) = GPIO_PIN_0;
+  // gpio_bit_reset(GPIOC, GPIO_PIN_0);
+  gpio_bit_write(GPIOC, GPIO_PIN_0, RESET);
+
   while (1) {
+    gpio_bit_toggle(GPIOC, GPIO_PIN_0);
+    delay_1ms(500);
   }
 }
