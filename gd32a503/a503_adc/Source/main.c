ed0e13724ca3b917854a52ed18f07ccff4d2f7f9
weifengdq <weifengdq@163.com>
2025-11-19 16:10:18 +0800

    a503 adc0_in1 pc10

--- gd32a503/a503_adc/Source/main.c
+++ gd32a503/a503_adc/Source/main.c
@@ -41,6 +41,13 @@ int fputc(int ch, FILE *f) {
   return ch;
 }
 
+void gpio_config(void) {
+  /* enable GPIOC clock */
+  rcu_periph_clock_enable(RCU_GPIOC);
+  /* config PC10 as analog input for ADC0_IN1 */
+  gpio_mode_set(GPIOC, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO_PIN_10);
+}
+
 void rcu_config(void) {
   /* enable ADC0 clock */
   rcu_periph_clock_enable(RCU_ADC0);
@@ -48,7 +55,6 @@ void rcu_config(void) {
   rcu_adc_clock_config(RCU_CKADC_CKAHB_DIV32);
 }
 
-static int32_t adc0_temp_calib_value = 0;
 void adc_config(void) {
   /* ADC mode config */
   adc_mode_config(ADC_MODE_FREE);
@@ -56,73 +62,59 @@ void adc_config(void) {
   adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);
   /* ADC continuous function disable */
   adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, DISABLE);
-  adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE);
+  adc_special_function_config(ADC0, ADC_SCAN_MODE, DISABLE);
 
   /* ADC channel length config */
-  adc_channel_length_config(ADC0, ADC_INSERTED_CHANNEL, 2);
-  /* ADC regular channel config */
-  adc_inserted_channel_config(ADC0, 0, ADC_CHANNEL_16,
-                              ADC_SAMPLETIME_479POINT5);
-  adc_inserted_channel_config(ADC0, 1, ADC_CHANNEL_17, ADC_SAMPLETIME_55POINT5);
+  adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 1);
+  /* ADC regular channel config - ADC0_IN1 (PC10) */
+  adc_regular_channel_config(ADC0, 0, ADC_CHANNEL_1, ADC_SAMPLETIME_55POINT5);
 
   /* config ADC trigger */
-  adc_external_trigger_source_config(ADC0, ADC_INSERTED_CHANNEL,
-                                     ADC0_1_EXTTRIG_INSERTED_NONE);
-  adc_external_trigger_config(ADC0, ADC_INSERTED_CHANNEL, ENABLE);
-
-  /* enable channel17(Vrefint), channel16(temperature sensor)  */
-  adc_vrefint_enable();
-  adc_tempsensor_enable();
+  adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL,
+                                     ADC0_1_EXTTRIG_REGULAR_NONE);
+  adc_external_trigger_config(ADC0, ADC_REGULAR_CHANNEL, ENABLE);
 
   /* enable ADC interface */
   adc_enable(ADC0);
   delay_1ms(1);
   /* ADC calibration and reset calibration */
   adc_calibration_enable(ADC0);
-
-  adc0_temp_calib_value = REG16(0x1FFFF7F8) & 0x0FFF;
 }
 
 int main(void) {
   systick_config();
   uart0_init(115200U);
+  gpio_config();
   rcu_config();
   adc_config();
 
-  printf("GD32A503 ADC Temperature Sensor and Vrefint Test\r\n");
+  printf("GD32A503 ADC0_IN1 (PC10) Test\r\n");
 
   int measure_cnt = 0;
-  uint32_t temp_sum = 0;
-  uint32_t vref_sum = 0;
+  uint32_t adc_sum = 0;
   while (1) {
-    adc_software_trigger_enable(ADC0, ADC_INSERTED_CHANNEL);
+    adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
     delay_1ms(20);
 
     // 50 次测量取平均值
-    if(measure_cnt < 50) {
-      uint16_t temp_val = adc_inserted_data_read(ADC0, 0);
-      uint16_t vref_val = adc_inserted_data_read(ADC0, 1);
-      temp_sum += temp_val;
-      vref_sum += vref_val;
+    if (measure_cnt < 50) {
+      uint16_t adc_val = adc_regular_data_read(ADC0);
+      adc_sum += adc_val;
       measure_cnt++;
     } else {
-      uint32_t avg_temp = temp_sum / 50;
-      uint32_t avg_vref = vref_sum / 50;
+      uint32_t avg_adc = adc_sum / 50;
 
       // 计算电压值，单位 mV
-      uint32_t vref_voltage = (uint32_t)(5000U * avg_vref) / 4095;
+      // Vref = 3.3V (3300mV)
+      uint32_t voltage = (uint32_t)(3300U * avg_adc) / 4095;
 
-      // 计算温度值，单位 摄氏度
-      // 温度传感器参数见参考手册
-      float temperature = (avg_temp - adc0_temp_calib_value) * 1000.0f * 5.0f /
-                            4095.0f / 4.58f + 30.0f;
-
-      printf("Vref: %" PRIu32 " mV, Temperature: %.2f C\r\n", vref_voltage, temperature);
+      printf("ADC0_IN1: %" PRIu32 " (ADC Value: %" PRIu32 "), Voltage: %" PRIu32
+             " mV\r\n",
+             avg_adc, avg_adc, voltage);
 
       // 重置测量
       measure_cnt = 0;
-      temp_sum = 0;
-      vref_sum = 0;
+      adc_sum = 0;
     }
   }
 }
